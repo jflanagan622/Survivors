4a996f4686c68963fc9eb7a51cd685248c5ec0e3
Sean <ekzaiura@yahoo.com>
2017-05-01 09:59:03 -0400

    Add files via upload

--- web_server.js
+++ web_server.js
@@ -4,9 +4,9 @@
 var express = require('express');
 var app = express();
 
-// Set the port to listen on. 80 since it's the web server
+// Set the port to listen on. 3001 since it's the web server
 // NOTE: su usually required for ports under 1024
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3001);
 
 // Setup to serve static files
 app.use(express.static(__dirname + '/public'));
@@ -29,7 +29,7 @@ app.disable('etag');
 //-----------------------------------------
 // Startup the server
 app.listen(app.get('port'), function(){
-	console.log( 'The Web Server is running. Open a browser and navigate to: http://localhost');
+	console.log( 'The Web Server is running. Open a browser and navigate to: http://localhost:/3001/');
 });
 
 
